550128ede435705faceb7d47078fe9e465f78e91
josef <youcefhaciane24@gmail.com>
2023-11-18 16:27:38 +0100

    change data3

--- projet/deployement.py
+++ projet/deployement.py
@@ -23,6 +23,19 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Database
+"""
+connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+parametrs = {pair.split('=')[0]: pair.split('=')[1] for pair in connection_string.split(' ')}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': parametrs['dbname'],
+        'HOST': parametrs['host'],
+        'USER': parametrs['user'],
+        'PASSWORD': parametrs['password'],
+    }
+}
+"""
 connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 parametrs = {pair.split('=')[0]: pair.split('=')[1] for pair in connection_string.split(' ')}
 DATABASES = {
